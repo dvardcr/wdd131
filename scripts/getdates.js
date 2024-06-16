7d6c85e09846e9d1412161ca77e8a08d0889c50b
Diego Vargas <diego.vargasd@gmail.com>
2024-06-15 19:15:46 -0600

    Update CSS

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,4 +3,6 @@ const today = new Date();
 year.textContent = `${today.getFullYear()}`;
 
 const lastModifiedElement = document.querySelector("#lastModified");
-lastModifiedElement.textContent = "Last Modified: " + document.lastModified;
+lastModifiedElement.textContent = "Last Modification: " + document.lastModified;
+lastModifiedElement.style.color = "gold";
+lastModifiedElement.style.fontSize = "0.75em";
