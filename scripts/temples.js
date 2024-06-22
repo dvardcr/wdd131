5d23fb5240963be61fa1e57294c4315f15627ffe
Diego Vargas <diego.vargasd@gmail.com>
2024-06-22 16:00:34 -0600

    Final Commit - W02 Assignment: Picture Album

--- scripts/temples.js
+++ scripts/temples.js
@@ -8,11 +8,36 @@ lastModifiedElement.style.color = "gold";
 lastModifiedElement.style.fontSize = "0.75em";
 
 
-/*Hamburguer Menu*/
+/* Hamburguer Menu */
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
+});
+
+// Header 2 - Menu Link
+document.addEventListener('DOMContentLoaded', function() {
+    const dynamicHeader = document.getElementById('dynamicHeader');
+
+    const defaultHeaderText = 'Home';
+    
+    if (dynamicHeader.textContent.trim() === '') {
+        dynamicHeader.textContent = defaultHeaderText;
+    }
+
+    const menuItems = document.querySelectorAll('.navigation ul li');
+
+    function handleMenuItemClick(event) {
+        event.preventDefault();
+
+        const menuItemText = event.target.textContent.trim();
+
+        dynamicHeader.textContent = menuItemText;
+    }
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', handleMenuItemClick);
+    });
 });
