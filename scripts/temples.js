02cd57561ea9c67c4a8d212567762747d55fac82
Diego Vargas <diego.vargasd@gmail.com>
2024-06-22 13:43:33 -0600

    Updated CSS and JS

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,3 +7,12 @@ lastModifiedElement.textContent = "Last Modification: " + document.lastModified;
 lastModifiedElement.style.color = "gold";
 lastModifiedElement.style.fontSize = "0.75em";
 
+
+/*Hamburguer Menu*/
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
